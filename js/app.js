66613ed6478ae978a295a0a6a1f282a0345baa2d
NafizAhamad <mdnafizahamed177@gmail.com>
2023-10-29 22:22:18 +0600

    commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// (function ($) {
+//     "use strict";
+
+//     jQuery(document).ready(function($){
+//         $(".homepage-slides").owlcarousel({
+//             items: 1,
+//             nav: false,
+//             dots: true,
+//             loop: true,
+//             autoplay: false
+//         });
+
+//     });
+
+//     jQuery(window).load(function(){
+
+//     });
+
+// }(jQuery));
